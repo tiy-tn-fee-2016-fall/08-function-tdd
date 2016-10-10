313580421b4d51d4f61112986034b11de5d8ba08
Isaac Weber <ikey2244@gmail.com>
2016-10-10 18:09:59 -0500

    should-hit

--- app/blackjack/should-hit.js
+++ app/blackjack/should-hit.js
@@ -2,5 +2,13 @@ import shouldSplit from './should-split';
 import cardSum from './card-sum';
 
 export default function (card1, card2) {
+  if (shouldSplit(card1, card2)) {
+    return false;
+  }
 
+  if (cardSum(card1, card2) <= 17) {
+    return true;
+  }
+
+  return false;
 }
