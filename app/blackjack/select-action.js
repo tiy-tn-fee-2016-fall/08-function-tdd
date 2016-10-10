3bce2db0f5de6bfd5a6fcc76fb4bff4baabe21f1
Isaac Weber <ikey2244@gmail.com>
2016-10-10 18:10:15 -0500

    select-action

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -2,5 +2,12 @@ import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
 export default function (card1, card2) {
+  if (shouldSplit(card1, card2)) {
+    return 'split';
+  }
+  if (shouldHit(card1, card2)) {
+    return 'hit';
+  }
 
+  return 'stay';
 }
