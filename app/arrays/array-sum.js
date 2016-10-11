6df3dd39f3ddcea7268b935e1cdb4cbba4b2f09d
“Shaun <shaunwmusic@gmail.com>
2016-10-11 18:57:42 -0500

    Deep equal test pass

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,3 +1,8 @@
 export default function arraySum(arr) {
+  var count = 0;
+  for (var i = 0; i < arr.length; i++) {
+    count = count + arr[i];
+  }
 
+  return count;
 }
--- app/arrays/deep-equal.js
+++ app/arrays/deep-equal.js
@@ -1,3 +1,17 @@
 export default function deepEquals(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  for (let j = 0; j < b.lenth; j++) {
+    if (a[j] !== b[j]) {
+      return false;
+    }
+  }
+  return true;
 }
