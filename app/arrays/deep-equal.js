653d588621f55f22a7ae9e2248ba69dd51036395
Isaac Weber <ikey2244@gmail.com>
2016-10-11 18:02:15 -0500

    deep-equal.js

--- app/arrays/deep-equal.js
+++ app/arrays/deep-equal.js
@@ -1,3 +1,19 @@
-export default function deepEquals(a, b) {
 
+export default function deepEquals(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  for (let i = 0; i < b.length; i++) {
+    if (b[i] !== a[i]) {
+      return false;
+    }
+
+    return true;
+  }
 }
