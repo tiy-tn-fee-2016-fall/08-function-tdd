9cca947080e09a91fc6e8742bdeae6acd9735256
Dustin Walker <dwtimp@gmail.com>
2016-10-10 19:31:53 -0500

    completed tests #2,3

--- app/blackjack/card-value.js
+++ app/blackjack/card-value.js
@@ -1,3 +1,30 @@
-export default function (cardName) {
+export default function (cardValue) {
+  const = five;
+  if (cardValue === '5') {
+    return five;
+  }
 
+  if (cardValue === '9') {
+    return nine;
+  }
+
+  if (cardValue === '10') {
+    return ten;
+  }
+
+  if (cardValue === 'jack') {
+    return 10;
+  }
+
+  if (cardValue === 'queen') {
+    return 10;
+  }
+
+  if (cardValue === 'king') {
+    return 10;
+  }
+
+  if (cardValue === 'ace') {
+    return 11;
+  }
 }
