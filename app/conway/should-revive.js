c712b452aa6436c2fe05b23781b2c4f4d580cfa1
“Shaun <shaunwmusic@gmail.com>
2016-10-12 13:49:42 -0500

    final test on conway pass

--- app/conway/should-revive.js
+++ app/conway/should-revive.js
@@ -1,7 +1,11 @@
 import aliveNeighbors from './alive-neighbors';
 
 export default function shouldStayAlive(neighbors) {
-  for (var i = 0; i < neighbors.length; i++) {
-    neighbors[i];
+  aliveNeighbors(neighbors);
+  if (aliveNeighbors(neighbors) <= 3 && aliveNeighbors(neighbors) >= 3) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
