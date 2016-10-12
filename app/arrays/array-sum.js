fc96a7a40163f00b7c59be226ac43996425e3870
Isaac Weber <ikey2244@gmail.com>
2016-10-12 15:27:08 -0500

    alive-neighbors.js

--- app/arrays/array-sum.js
+++ app/arrays/array-sum.js
@@ -1,3 +1,10 @@
-export default function arraySum(arr) {
 
+export default function (arr) {
+  let arraySum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    arraySum = arraySum + arr[i];
+  }
+
+  return arraySum;
 }
--- app/conway/should-stay-alive.js
+++ app/conway/should-stay-alive.js
@@ -1,5 +1,13 @@
 import aliveNeighbors from './alive-neighbors';
 
 export default function shouldStayAlive(neighbors) {
+  let alive = 0;
 
+  for (let i = 0; i < neighbors.length; i++) {
+    if (neighbors[i] === true) {
+      alive++;
+    }
+  }
+
+  return alive;
 }
