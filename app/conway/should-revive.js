64b59e3e4bdc2423d964348bf41fc890af079e09
Dustin Walker <dwtimp@gmail.com>
2016-10-18 20:03:48 -0500

    stuck

--- app/conway/should-revive.js
+++ app/conway/should-revive.js
@@ -1,5 +1,10 @@
 import aliveNeighbors from './alive-neighbors';
 
 export default function shouldStayAlive(neighbors) {
-
+  if (neighbors === 3) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
