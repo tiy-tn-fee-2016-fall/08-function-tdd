bc8ecf5c3b2461e0e45d1d85ba4d17b4e6c8aecd
Dustin Walker <dwtimp@gmail.com>
2016-10-18 19:40:47 -0500

    all braces matter

--- app/conway/should-stay-alive.js
+++ app/conway/should-stay-alive.js
@@ -1,10 +1,13 @@
 import aliveNeighbors from './alive-neighbors';
 
 export default function shouldStayAlive(neighbors) {
-  let alive = 0;
-  if (aliveNeighbors(neighbors) <= 3) {
+  if (aliveNeighbors(neighbors) < 2) {
     return false;
   }
-
-  return alive;
+  if (aliveNeighbors(neighbors) > 3) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
